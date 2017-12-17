c4866b9b113c04cfaadf5a4c01d972421929a479
i-den <idenchev07@gmail.com>
2017-12-17 21:19:05 +0200

    Fixed Category name to never be blank

--- src/AppBundle/Entity/Category.php
+++ src/AppBundle/Entity/Category.php
@@ -4,6 +4,7 @@ namespace AppBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Category
@@ -26,6 +27,10 @@ class Category
      * @var string
      *
      * @ORM\Column(name="name", type="string", length=255)
+     *
+     * @Assert\NotBlank(
+     *     message="Category's name cannot be empty"
+     * )
      */
     private $name;
 
